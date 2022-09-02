469e1cd0e678ce0afa15e97b807ab76140cb1d49
JuKoNi <92727651+JuKoNi@users.noreply.github.com>
2022-09-02 13:51:46 +0200

    added modules

---
+++ modules/Hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Hero = (props: Props) => {
+  return (
+    <section>
+      <img src="../src/assets/hero.jpg" alt="Picture of a bunch of dogs in front of a pink wall" />
+    </section>
+  )
+}
+
+export default Hero
