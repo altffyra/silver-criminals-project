7780e36f07ad42ff8ae63eee72bf07965df8f74e
JuKoNi <92727651+JuKoNi@users.noreply.github.com>
2022-09-06 13:21:37 +0200

    header/hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 
 import { useState } from 'react';
 import './App.css';
-import Header from '../modules/Header';
-import Hero from '../modules/Hero';
-import Filter from '../modules/Filter';
-import Animals from '../modules/Animals';
-import CallToAction from '../modules/CallToAction';
-import Footer from '../modules/Footer';
+import Header from './modules/Header';
+import Hero from './modules/Hero';
+import Filter from './modules/Filter';
+import Animals from './modules/Animals';
+import CallToAction from './modules/CallToAction';
+import Footer from './modules/Footer';
 import {animals} from './context.json'
 
 function App() {
