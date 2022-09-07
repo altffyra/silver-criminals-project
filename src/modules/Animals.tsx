dfcfb5cdfa9788534b5c5d73ec34a3cef1abcce8
JuKoNi <92727651+JuKoNi@users.noreply.github.com>
2022-09-07 13:10:47 +0200

    card css

--- src/modules/Animals.tsx
+++ src/modules/Animals.tsx
@@ -18,23 +18,29 @@ const Animals = (props: Props) => {
   }
 
   return (
-    <div className='animalCard'>
-      <div className='pictureFrame'>
-      <img className='imageMedium' src={props.info.picture}></img>
-      </div>
-      <h1 className='animalName'>{props.info.name}</h1>
-      <div className='animalSpecs'>
-        <p>Gender: {animalGender}</p>
-        <p>Size: {props.info.size}</p>
-        <p>Type: {props.info.type}</p>
-        <p>Race: {props.info.race}</p>
-        <p>Age: {props.info.age}</p>
-        <p>Location: {props.info.location}</p>
-        <p>Good with: {props.info.WellWith} </p>
-        <p>Energy-Level: {props.info.Energy}</p>
-      </div>
-    <button className='readMore-button' onClick={showAnimal}> Läs mer! </button>
-    </div>
+
+      <section className='animalCard'>
+
+        <img className='imageMedium' src={props.info.picture}></img>
+
+        <h1 className='animalName'>{props.info.name}</h1>
+
+        <div className='animalSpecs'>
+          <p>Kön: {animalGender}</p>
+          <p>Storlek: {props.info.size}</p>
+          <p>Typ: {props.info.type}</p>
+          <p>Ras: {props.info.race}</p>
+          <p>Ålder: {props.info.age}</p>
+          <p>Finns i: {props.info.location}</p>
+          <p>Funkar med: {props.info.WellWith} </p>
+          <p>Aktivitetsnivå: {props.info.Energy}</p>
+        </div>
+
+        <div className='separator'></div>
+
+        <button className='readMore-button' onClick={showAnimal}> Läs mer! </button>
+      </section>
+
   )
 }
 
