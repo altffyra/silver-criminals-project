52364afeff0bde963c4ad14f10cd7b62cac3d8cf
JuKoNi <92727651+JuKoNi@users.noreply.github.com>
2022-09-07 09:18:28 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,10 @@ import Filter from './modules/Filter';
 import Animals from './modules/Animals';
 import CallToAction from './modules/CallToAction';
 import Footer from './modules/Footer';
-import {animals} from './context.json'
-import {Animalinterface} from '../modules/interfaces'
+import {animals} from './context.json';
+import {Animalinterface} from './modules/interfaces';
+
+
 function App() {
 
 
