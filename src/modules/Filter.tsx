f56dc53f39e71ceb402f81ecfe44f5416a160ea9
Christian Johnsson <chjohnsson@hotmail.com>
2022-09-09 15:48:26 +0200

    filter algoritm

--- src/modules/Filter.tsx
+++ src/modules/Filter.tsx
@@ -1,69 +1,85 @@
 import React, { ChangeEvent, FormEvent } from 'react'
-import {Animalinterface} from '../modules/interfaces'
+import {Animalinterface, FilterInterface} from '../modules/interfaces'
+import { SetStateAction, useState } from 'react';
 
 interface Props { 
   info:Animalinterface[];
   update: any
   }
+
+  
+
+
 type FormType =  ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>
 
+// type FilterGenderType = "gender" | "race"
 
 const Filter = (props: Props) => {
 
-  let filterSearch:any= {}
-  function changeForm(event:FormType){
-    if (event.target.value== "Hane"|| event.target.value== "Hona" ) {
-      filterSearch.gender = event.target.value
-  }    
-    else if(event.target.value== "genderAny"){
-      delete filterSearch.gender
-    }
-  console.log(filterSearch)
-  }
+  const [filter, setFilter] = useState<FilterInterface | any>({})
+ 
+  function changeForm(e:FormType){
 
+    if (e.target.value == "Any") {
+      const prop = e.target.name;
+      delete filter[prop]
+      console.log(filter)
+  }
+    else {
+    setFilter({
+      ...filter,
+      [e.target.name]: e.target.value
+    })
+    console.log(filter)
+  }
+  }
   return (
     <div className='outerFilterWrapper' id="search">
       <h2>Sök efter:</h2>
     <div className='filterWrapper'> 
       <div className='left-form'>
-        <p>Plats</p> <input type="text" name="Location" onChange={changeForm}></input>
+        <p>Plats</p> <input type="text" name="Location" onChange={()=> changeForm(event)}></input>
         <div className='searchForms'>
           <p>Gender</p> 
-          <select onChange={changeForm}>
-          <option value="genderAny" >Alla</option>
+          <select name='gender' onChange={()=> changeForm(event)}>
+          <option value="Any" >Alla</option>
           <option value="Hane">Hane</option>
           <option value="Hona">Hona</option>
-        </select>
+          </select>
         <p>Storlek</p> 
-          <select onChange={changeForm}>
-          <option value="sizeAny">Alla</option>
+          <select name='size'onChange={()=> changeForm(event)}>
+          <option value="Any">Alla</option>
           <option value="Liten">Liten</option>
           <option value="Medium">Medium</option>
           <option value="Stor">Stor</option>
         </select>
-      <p>Typ</p> <select onChange={changeForm}>
-        <option value="typeAny">Alla</option>
+      <p>Typ</p> <select name='type' onChange={()=> changeForm(event)}>
+        <option value="Any">Alla</option>
         <option value="Katt">Katt</option>
         <option value="Hund">Hund</option>
         <option value="other">Annat</option>
       </select>
       </div>
       <div className='right-form'>
-      <p>Bra med små barn</p> <select onChange={changeForm}>
-      <option value="goodWithAnySmall">Ingen Preferens</option>
-      <option value="goodWithChildrenUnder5">Bra</option>
+      <p>Bra med små barn</p> 
+      <select name='WellWith' onChange={()=> changeForm(event)}>
+      <option value="Any">Ingen Preferens</option>
+      <option value="Små barn">Bra</option>
       </select>
-      <p>Bra med stora barn</p> <select onChange={changeForm}>
-      <option value="goodWithAnyLarge">Ingen Preferens</option>
-      <option value="goodWithChildrenOver5">Bra</option>
+      <p>Bra med stora barn</p> 
+      <select name='WellWith' onChange={()=> changeForm(event)}>
+      <option value="Any">Ingen Preferens</option>
+      <option value="Stora barn">Bra</option>
       </select>
-      <p>Bra med katter</p> <select onChange={changeForm}>
-      <option value="goodWithCatsAny">Ingen Preferens</option>
-      <option value="goodWithCats">Bra</option>
+      <p>Bra med katter</p> 
+      <select  name='WellWith' onChange={()=> changeForm(event)}>
+      <option value="Any">Ingen Preferens</option>
+      <option value="Katter">Bra</option>
       </select>
-      <p>Bra med hundar</p> <select onChange={changeForm}>
-      <option value="goodWithDogsAny">Ingen Preferens</option>
-      <option value="goodWithDogs">Bra</option>
+      <p>Bra med hundar</p> 
+      <select  name='WellWith' onChange={()=> changeForm(event)}>
+      <option value="Any">Ingen Preferens</option>
+      <option value="Hundar">Bra</option>
       
       </select>
       </div>
