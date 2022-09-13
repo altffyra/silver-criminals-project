250107122d57e3f2ea270f774aa2684cb026ada7
ammarfolk <ammar.mawaldi@gmail.com>
2022-09-13 09:48:27 +0200

    adoption form
    almost done

--- src/modules/AdoptionForm.tsx
+++ src/modules/AdoptionForm.tsx
@@ -17,9 +17,6 @@ const AdoptionForm = (props: Props) => {
 
         <article className="adopt__card">
           <form className="adopt__form">
-
-
-
             <section className="adopt__user">
               <label>
                 Din För- och efternamn:
@@ -51,7 +48,6 @@ const AdoptionForm = (props: Props) => {
 
             <section className="divider"></section>
 
-            
             <label>
               Djurets namn:
               <input
@@ -62,127 +58,128 @@ const AdoptionForm = (props: Props) => {
             </label>
 
             <label>
-            Plats:
+              Plats:
               <input className="adopt__input" name="Location"></input>
             </label>
 
             <section className="adopt__wrapper">
-            <section className="adopt__img">
-              <img src="" alt="" />
+              <section className="adopt__img">
+                <img src="" alt="" />
+              </section>
+
+              <section>
+                <label className="adopt__animal">
+                  kön:
+                  <select className="options" name="gender">
+                    <option value="Any">Alla</option>
+                    <option value="Hane">Hane</option>
+                    <option value="Hona">Hona</option>
+                  </select>
+                </label>
+
+                <label className="adopt__animal">
+                  Storlek:
+                  <select className="options" name="size">
+                    <option value="alla">Alla</option>
+                    <option value="Liten">Liten</option>
+                    <option value="Medium">Medium</option>
+                    <option value="Stor">Stor</option>
+                  </select>
+                </label>
+
+                <label className="adopt__animal">
+                  Typ:
+                  <select className="options" name="type">
+                    <option value="Any">Alla</option>
+                    <option value="Katt">Katt</option>
+                    <option value="Hund">Hund</option>
+                    <option value="other">Annat</option>
+                  </select>
+                </label>
+
+                <label className="adopt__animal">
+                  Ras
+                  <select className="options" name="Race">
+                    <option value="Any">Alla</option>
+                    <option value="option 2">2</option>
+                  </select>
+                </label>
+
+                <label className="adopt__animal">
+                  Energi:
+                  <select className="options" name="Energy">
+                    <option value="Any">Alla</option>
+                    <option value="option 2">2</option>
+                  </select>
+                </label>
+
+                <label className="adopt__animal">
+                  Ålder:
+                  <select className="options" name="Old">
+                    <option value="Any">Alla</option>
+                    <option value="option 2">2</option>
+                  </select>
+                </label>
+              </section>
+
+              <section>
+                <label className="adopt__specs">
+                  Bra med små barn:
+                  <select className="options" name="WellWithSmall">
+                    <option value="Any">Ingen Preferens</option>
+                    <option value="Små barn">Bra</option>
+                  </select>
+                </label>
+
+                <label className="adopt__specs">
+                  Bra med stora barn:
+                  <select className="options" name="WellWithLarge">
+                    <option value="Any">Ingen Preferens</option>
+                    <option value="Stora barn">Bra</option>
+                  </select>
+                </label>
+
+                <label className="adopt__specs">
+                  Bra med katter:
+                  <select className="options" name="WellWithCats">
+                    <option value="Any">Ingen Preferens</option>
+                    <option value="Katter">Bra</option>
+                  </select>
+                </label>
+
+                <label className="adopt__specs">
+                  Bra med hundar:
+                  <select className="options" name="WellWithDogs">
+                    <option value="Any">Ingen Preferens</option>
+                    <option value="Hundar">Bra</option>
+                  </select>
+                </label>
+              </section>
             </section>
 
-            <section>
-              <label className="adopt__animal">
-              kön:
-                <select className="options" name="gender">
-                  <option value="Any">Alla</option>
-                  <option value="Hane">Hane</option>
-                  <option value="Hona">Hona</option>
-                </select>
-              </label>
-
-              <label className="adopt__animal">
-              Storlek:
-                <select className="options" name="size">
-                  <option value="alla">Alla</option>
-                  <option value="Liten">Liten</option>
-                  <option value="Medium">Medium</option>
-                  <option value="Stor">Stor</option>
-                </select>
-              </label>
-
-              <label className="adopt__animal">
-              Typ:
-                <select className="options" name="type">
-                  <option value="Any">Alla</option>
-                  <option value="Katt">Katt</option>
-                  <option value="Hund">Hund</option>
-                  <option value="other">Annat</option>
-                </select>
-              </label>
-
-              <label className="adopt__animal">
-              Ras
-                <select className="options" name="Race">
-                  <option value="Any">Alla</option>
-                  <option value="option 2">2</option>
-                </select>
-              </label>
-
-              <label className="adopt__animal">
-              Energi:
-                <select className="options" name="Energy">
-                <option value="Any">Alla</option>
-                  <option value="option 2">2</option>
-                </select>
-              </label>
-
-              <label className="adopt__animal">
-              Ålder:
-                <select className="options" name="Old">
-                <option value="Any">Alla</option>
-                  <option value="option 2">2</option>
-                </select>
-              </label>
-            </section>
-
-            <section>
-              <label  className="adopt__specs">
-              Bra med små barn:
-                <select className="options" name="WellWithSmall">
-                  <option value="Any">Ingen Preferens</option>
-                  <option value="Små barn">Bra</option>
-                </select>
-              </label>
-
-              <label className="adopt__specs">
-              Bra med stora barn:
-                <select className="options" name="WellWithLarge">
-                  <option value="Any">Ingen Preferens</option>
-                  <option value="Stora barn">Bra</option>
-                </select>
-              </label>
-
-              <label className="adopt__specs">
-              Bra med katter:
-                <select className="options" name="WellWithCats">
-                  <option value="Any">Ingen Preferens</option>
-                  <option value="Katter">Bra</option>
-                </select>
-              </label>
-
-              <label className="adopt__specs">
-              Bra med hundar:
-                <select className="options" name="WellWithDogs">
-                  <option value="Any">Ingen Preferens</option>
-                  <option value="Hundar">Bra</option>
-                </select>
-              </label>
-            </section>
-            </section>
-
-
-            <label>
+            <label className="adopt__about__animal">
               Lite om djuret:
-              <input
+              <textarea
                 name="userPhone"
                 className="adopt__about"
-                type="text"
-              ></input>
+              ></textarea>
+
+
             </label>
 
-            <button onClick={props.switchToAdoption} className="adopt__btn">
-            skicka!
-            <img
-              className="adopt__btn__logo"
-              src="../src/assets/logo.png"
-              alt=""
-            />
-          </button>
-          </form>
 
+          </form>
+          <button onClick={props.switchToAdoption} className="adopting__btn">
+          Skicka anmälan
+                        <img
+                className="adopting__btn__logo"
+                src="../src/assets/logo.png"
+                alt=""
+              />
+            </button>
 
         </article>
+
       </div>
     </div>
   );
